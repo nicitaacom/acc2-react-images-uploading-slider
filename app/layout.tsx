1357a1c2634659bc3e1e143a60d003dea0004370
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-12-12 10:56:09 +0100

    +-upd: title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,21 @@
-import './globals.css'
+import "./globals.css"
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 import Layout from "./components/Layout"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'codesandbox project',
-  description: 'description',
+  title: "acc2-react-images-uploading-slider",
+  description: "some description here",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Layout>
-          {children}
-        </Layout>
+        <Layout>{children}</Layout>
       </body>
     </html>
   )
